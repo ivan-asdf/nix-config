0bd62333cfa3cf1f9816a176c2d7826e36e4186b
Ivan <ivan.asdf0@gmail.com>
2023-03-02 21:17:35 +0200

    Added fancy prompt 'Starship'

--- home/home.nix
+++ home/home.nix
@@ -19,7 +19,17 @@
     extraConfig.init.defaultBranch = "master";
   };
 
-  programs.bash.enable = true;
+  programs.bash = {
+    enable = true;
+    bashrcExtra = builtins.readFile ./bashrc;
+  };
+
+  programs.starship = {
+    enable = true;
+  };
+  xdg.configFile."starship.toml" = {
+    source = ./starship.toml;
+  };
 
   programs.neovim = {
     enable = true;
