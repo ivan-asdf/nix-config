6fe3b42f7818e7a39d290725ce087413818d3210
Ivan <ivan.asdf0@gmail.com>
2023-03-08 14:55:08 +0200

    NvimTree: auto close nvim-tree window if last window.

--- home/programs/nvim/nvim_config/init.lua
+++ home/programs/nvim/nvim_config/init.lua
@@ -33,6 +33,15 @@ local function open_nvim_tree(data)
   require("nvim-tree.api").tree.open()
 end
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+-- auto close if last window https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close 
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+      vim.cmd "quit"
+    end
+  end
+})
 -- have explored all optionns upto nvim-tree.trash
 require("nvim-tree").setup({
   disable_netrw = true,
