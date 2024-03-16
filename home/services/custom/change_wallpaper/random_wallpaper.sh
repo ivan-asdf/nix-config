33f80ad2087b87b4a2f27399b7b31781421b7647
Ivan <ivan.asdf0@gmail.com>
2024-03-16 21:47:32 +0200

    Added wallpapers.

--- home/services/custom/change_wallpaper/random_wallpaper.sh
+++ home/services/custom/change_wallpaper/random_wallpaper.sh
@@ -1,7 +1,15 @@
-wallpapers=~/.config/wallpapers
-pushd $wallpapers
-wallpaper=$(ls . | sort -R | tail -1)
-ln -s -f $wallpaper .wallpaper
-#feh --bg-fill .wallpaper; rm ~/.fehbg
-betterlockscreen -u .wallpaper --fx blur
+#!/bin/bash
+set -e
+
+if [ $# -eq 1 ]; then
+    WALLPAPER="$1"
+else
+    WALLPAPERS_DIR=~/.config/wallpapers
+    pushd $WALLPAPERS_DIR
+
+    WALLPAPER=$(find . -type l | sort -R | tail -1)
+    #ln -s -f "$WALLPAPER" .wallpaper
+fi
+
+betterlockscreen -u "$WALLPAPER" --fx blur
 betterlockscreen -w
